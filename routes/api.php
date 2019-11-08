fba17690f8bc765a6bf93d56284d670426cc8a24
Basel Shoban <basel.sh@likesyria.sy>
2019-11-08 02:28:59 +0200

    Add Article Entity

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 Route::post('login', 'AuthController@login');
 Route::apiresource('category', 'CategoryController');
+Route::apiresource('article', 'ArticleController');
