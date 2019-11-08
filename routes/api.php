56449fbda816428ca06ff54dfa87681c62aefcba
Basel Shoban <basel.sh@likesyria.sy>
2019-11-08 22:41:16 +0200

    Remove extra route

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 Route::post('login', 'AuthController@login');
 Route::apiresource('category', 'CategoryController');
 Route::apiresource('article', 'ArticleController');
